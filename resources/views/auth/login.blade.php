807bb34b6e54ee0add5f68bde31d8db9526dc81b
ajil23 <azrilpraya@gmail.com>
2024-07-09 14:41:44 +0800

    login view with logout mechanism

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,123 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+<!doctype html>
+<html lang="en">
+
+    
+<!-- Mirrored from themesdesign.in/webadmin/layouts/auth-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Apr 2024 02:26:48 GMT -->
+<head>
+
+        <meta charset="utf-8" />
+        <title>Login </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    
+    <body>
+
+    <!-- <body data-layout="horizontal"> -->
+
+    <div class="authentication-bg min-vh-100">
+        <div class="bg-overlay bg-light"></div>
+        <div class="container">
+            <div class="d-flex flex-column min-vh-100 px-3 pt-4">
+                <div class="row justify-content-center my-auto">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+
+                        <div class="mb-4 pb-2">
+                            <a href="index.html" class="d-block auth-logo">
+                                <img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="30" class="auth-logo-dark me-start">
+                                <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="30" class="auth-logo-light me-start">
+                            </a>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-body p-4"> 
+                                <div class="text-center mt-2">
+                                    <h5>Welcome Back !</h5>
+                                    <p class="text-muted">Sign in to continue to webadmin.</p>
+                                </div>
+                                <div class="p-2 mt-4">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label" for="email">Email</label>
+                                            <div class="position-relative input-custom-icon">
+                                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                 <span class="bx bx-user"></span>
+                                            </div>
+                                        </div>
+                
+                                        <div class="mb-3">
+                                            <div class="float-end">
+                                                <a href="auth-recoverpw.html" class="text-muted text-decoration-underline">Forgot password?</a>
+                                            </div>
+                                            <label class="form-label" for="password-input">Password</label>
+                                            <div class="position-relative auth-pass-inputgroup input-custom-icon">
+                                                <span class="bx bx-lock-alt"></span>
+                                                <input type="password" class="form-control" id="password-input" placeholder="Enter password" name="password" required autocomplete="current-password">
+                                                <button type="button" class="btn btn-link position-absolute h-100 end-0 top-0" id="password-addon">
+                                                    <i class="mdi mdi-eye-outline font-size-18 text-muted"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                
+                                        <div class="form-check py-1">
+                                            <input type="checkbox" class="form-check-input" id="auth-remember-check">
+                                            <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                        </div>
+                                        
+                                        <div class="mt-3">
+                                            <button class="btn btn-dark w-100 waves-effect waves-light" type="submit">Log In</button>
+                                        </div>
+
+                                    </form>
+                                </div>
+            
+                            </div>
+                        </div>
+
+                    </div><!-- end col -->
+                </div><!-- end row -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center p-4">
+                            <p>© <script>document.write(new Date().getFullYear())</script> webadmin. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div><!-- end container -->
+    </div>
+    <!-- end authentication section -->
+
+        <!-- JAVASCRIPT -->
+        <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('assets/libs/metismenujs/metismenujs.min.js')}}"></script>
+        <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('assets/libs/eva-icons/eva.min.js')}}"></script>
+
+        <script src="{{asset('assets/js/pages/pass-addon.init.js')}}"></script>
+
+    </body>
+
+
+<!-- Mirrored from themesdesign.in/webadmin/layouts/auth-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Apr 2024 02:26:48 GMT -->
+</html>
