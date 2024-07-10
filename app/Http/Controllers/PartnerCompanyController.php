4fb42b2945aad0b03ecdab9117804a442982c1d3
ajil23 <azrilpraya@gmail.com>
2024-07-10 15:44:12 +0800

    fixing

--- app/Http/Controllers/PartnerCompanyController.php
+++ app/Http/Controllers/PartnerCompanyController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PartnerCompanyController extends Controller
 {
     public function index(){
-        return view('partner.company.add');
+        return view('partner.company.index');
     }
 }
--- app/Http/Controllers/PartnerFastboatController.php
+++ app/Http/Controllers/PartnerFastboatController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PartnerFastboatController extends Controller
 {
     public function index(){
-        return view('partner.fastboat.add');
+        return view('partner.fastboat.index');
     }
 }
