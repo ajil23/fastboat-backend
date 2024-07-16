6d99140f4227438fb50501b10af9b720fbbccad9
ajil23 <azrilpraya@gmail.com>
2024-07-16 10:12:58 +0800

    insert data to fastboat table

--- app/Http/Controllers/PartnerFastboatController.php
+++ app/Http/Controllers/PartnerFastboatController.php
@@ -2,23 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PartnerCompany;
+use App\Models\PartnerFastboat;
 use Illuminate\Http\Request;
 
 class PartnerFastboatController extends Controller
 {
     // this function is for view all data from fastboat table
     public function index(){
-        return view('partner.fastboat.index');
+        $fastboat = PartnerFastboat::paginate(10);
+        return view('partner.fastboat.index', compact('fastboat'));
     }
 
     // this function is for view form to add fastboat data
     public function add(){
-        return view('partner.fastboat.add');
+        $company = PartnerCompany::all();
+        return view('partner.fastboat.add', compact('company'));
     }
 
     // this function will request data from input in fast boat add form
     public function store(Request $request){
+        // Handle the request data validation
+        $request->validate([
+            'fb_name' => 'required',
+            'fb_company' => 'required',
+            'fb_image1' => 'required|image|mimes:jpeg,jpg,png|max:5120',
+            'fb_image2' => 'required|image|mimes:jpeg,jpg,png|max:5120',
+            'fb_image3' => 'required|image|mimes:jpeg,jpg,png|max:5120',
+            'fb_keywords' => 'required',
+            'fb_slug_en' => 'required',
+            'fb_slug_idn' => 'required',
+            'fb_description_en' => 'required',
+            'fb_description_idn' => 'required',
+            'fb_content_en' => 'required',
+            'fb_content_idn' => 'required',
+            'fb_status' => 'required',
+            'fb_content_en' => 'required',
+        ]);
 
+        // Handle insert data to database
+        $fastboatData = new PartnerFastboat();
+        $fastboatData -> fb_name = $request->fb_name;
+        $fastboatData -> fb_company = $request->fb_company;
+        $fastboatData -> fb_keywords = $request->fb_keywords;
+        $fastboatData -> fb_slug_en = $request->fb_slug_en;
+        $fastboatData -> fb_slug_idn = $request->fb_slug_idn;
+        $fastboatData -> fb_description_en = $request->fb_description_en;
+        $fastboatData -> fb_description_idn = $request->fb_description_idn;
+        $fastboatData -> fb_content_en = $request->fb_content_en;
+        $fastboatData -> fb_content_idn = $request->fb_content_idn;
+        $fastboatData -> fb_status = $request->fb_status;
+        $fastboatData -> fb_updated_by = Auth()->id();
+
+        // handle image store
+        if ($request->hasFile('fb_image1')) {
+            $fbimg1 = $request->file('fb_image1')->store('fb_image1');
+            $fastboatData->fb_image1 = $fbimg1;
+        }
+        if ($request->hasFile('fb_image2')) {
+            $fbimg2 = $request->file('fb_image2')->store('fb_image2');
+            $fastboatData->fb_image2 = $fbimg2;
+        }
+        if ($request->hasFile('fb_image3')) {
+            $fbimg3 = $request->file('fb_image3')->store('fb_image3');
+            $fastboatData->fb_image3 = $fbimg3;
+        }
+        if ($request->hasFile('fb_image4')) {
+            $fbimg4 = $request->file('fb_image4')->store('fb_image4');
+            $fastboatData->fb_image4 = $fbimg4;
+        }
+        if ($request->hasFile('fb_image5')) {
+            $fbimg5 = $request->file('fb_image5')->store('fb_image5');
+            $fastboatData->fb_image5 = $fbimg5;
+        }
+        if ($request->hasFile('fb_image6')) {
+            $fbimg6 = $request->file('fb_image6')->store('fb_image6');
+            $fastboatData->fb_image6 = $fbimg6;
+        }
+        $fastboatData->save();
+        toast('Your data as been submited!','success');
+        return redirect()->route('fastboat.view');
     }
     
     // this function will get the $id of the selected data and then view the fast boat edit form
@@ -33,6 +96,15 @@ class PartnerFastboatController extends Controller
 
     // this function will get the $id of selected data and do delete operation
     public function delete($id){
+        $fastboatData = PartnerFastboat::find($id);
+        $fastboatData -> delete();
+        toast('Your data as been deleted!','success');
+        return redirect()->route('fastboat.view');
+    }
 
+    // this function will get $id of selected data and view it in modal
+    public function show($id){
+        $fastboatData = PartnerFastboat::find($id);
+        return response()->json($fastboatData);
     }
 }
--- resources/views/partner/fastboat/index.blade.php
+++ resources/views/partner/fastboat/index.blade.php
@@ -21,53 +21,48 @@
                                 <table class="table table-striped table-centered align-middle table-nowrap mb-0 table-check">
                                     <thead>
                                         <tr>
+                                            <th>No</th>
                                             <th style="width: 90px;">
-                                               Product
+                                               Picture
                                             </th>
-                                            <th  style="width: 210px;">Product Name</th>
-                                            <th>Customer Name</th>
-                                            <th>Order ID</th>
-                                            <th>Color</th>
-                                            <th>Date</th>
+                                            <th  style="width: 210px;">Fast Boat Name</th>
+                                            <th>Company Name</th>
                                             <th>Status</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        @foreach ($fastboat as $item)
                                         <tr>
+                                            <td>{{$loop->iteration}}</td>
                                             <td>
                                                 <div class="avatar">
                                                     <div class="product-img avatar-title img-thumbnail bg-primary-subtle  border-0">
-                                                        <img src="assets/images/product/img-1.png" class="img-fluid" alt="">
+                                                        <img src="{{ asset('storage/'.$item->fb_image1) }}" class="img-fluid" alt="">
                                                     </div>
                                                 </div>
                                             </td>
-                                            <td class="fw-semibold">Office Chair Crime</td>
+                                            <td class="fw-semibold">{{$item->fb_name}}</td>
                                             <td>
-                                                Neal Matthews
+                                                {{$item->partnercompany->cpn_name}}
                                             </td>
                                             <td>
-                                            #526552
+                                                <span class="badge bg-primary-subtle text-primary  font-size-12">{{$item->fb_status}}</span>
                                             </td>
-                                            <td>
-                                                Gray
-                                            </td>
-                                            <td>12/01/2022</td>
-                                            <td><span class="badge bg-primary-subtle text-primary  font-size-12">Pending</span></td>
                                             <td>
                                                 <div class="dropdown">
                                                     <a class="text-muted dropdown-toggle font-size-18" role="button" data-bs-toggle="dropdown" aria-haspopup="true">
                                                         <i class="mdi mdi-dots-horizontal"></i>
                                                     </a>
                                                     <div class="dropdown-menu dropdown-menu-end">
-                                                        <a class="dropdown-item" href="#">View</a>
+                                                        <a class="dropdown-item" href="javascript:void(0)" id="showDetail" data-url="{{route('fastboat.show', $item->fb_id)}}">View</a>
                                                         <a class="dropdown-item" href="#">Edit</a>
-                                                        <a class="dropdown-item" href="#">Delete</a>
+                                                        <a class="dropdown-item" onclick="return confirm('Are you sure?')" href="{{route('fastboat.delete', $item->fb_id)}}">Delete</a>
                                                     </div>
                                                 </div>
                                             </td>
                                         </tr>
-                                        
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -80,5 +75,43 @@
     </div>
     <!-- End Page-content -->
 
+    <!-- Scrollable modal -->
+    <div class="modal fade" id="viewDetailModal" tabindex="-1" role="dialog"
+    aria-labelledby="viewDetailModalTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewDetailModalTitle">Company Information</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p><strong>Name : </strong><span id="fastboat-name"></span></p>
+                <p><strong>Keywords : </strong><span id="fastboat-keywords"></span></p>
+                <p><strong>Status : </strong><span id="fastboat-status"></span></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
     @include('admin.components.footer')
 </div>
+
+@section('script')
+{{-- javascript to get data from database & view in modal --}}
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('body').on('click', '#showDetail', function(){
+            var detailURL = $(this).data('url');
+            $.get(detailURL, function(data){
+                $('#viewDetailModal').modal('show');
+                    $('#fastboat-name').text(data.fb_name);
+                    $('#fastboat-keywords').text(data.fb_keywords);
+                    $('#fastboat-status').text(data.fb_status);
+            })
+        })
+    });
+</script>
+@endsection
