51c6b041735de836944c5fbd9781af1829d0eb2b
ernakha <ernakhasanah78@gmail.com>
2024-07-26 16:13:13 +0700

    view read, and add for trip

---
+++ resources/views/schedules/trip/add.blade.php
@@ -0,0 +1,254 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <form action="{{route('trip.store')}}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div id="addproduct-accordion" class="custom-accordion">
+                            <div class="card">
+                                <a href="#addproduct-productinfo-collapse" class="text-body" data-bs-toggle="collapse" aria-expanded="true" aria-controls="addproduct-productinfo-collapse">
+                                    <div class="p-4">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-shrink-0 me-3">
+                                                <div class="avatar">
+                                                    <div class="avatar-title rounded-circle bg-dark-subtle  text-dark">
+                                                        <h5 class="text-dark font-size-17 mb-0">01</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="font-size-16 mb-1">Trip Info</h5>
+                                                <p class="text-muted text-truncate mb-0">Fill all information below</p>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <i class="mdi mdi-chevron-up accor-down-icon font-size-24"></i>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </a>
+
+                                <div id="addproduct-productinfo-collapse" class="collapse show" data-bs-parent="#addproduct-accordion">
+                                    <div class="p-4 border-top">
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_name">Name</label>
+                                                    <input id="isd_name" name="isd_name" placeholder="Enter Island Name" type="text" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_code">Code</label>
+                                                    <input id="isd_code" name="isd_code" placeholder="Enter Island Code" type="text" class="form-control" required>
+                                                    </input>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_map">Maps</label>
+                                                    <input id="isd_map" name="isd_map" placeholder="Longitude,Latitude" type="text" class="form-control" required>
+                                                    </input>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_content_en">Content (en)</label>
+                                                    <textarea class="form-control" id="isd-content-en" name="isd_content_en" placeholder="Enter Content" rows="4"></textarea>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_content_idn">Content (idn)</label>
+                                                    <textarea class="form-control" id="isd-content-idn" name="isd_content_idn" placeholder="Enter Content" rows="4"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <a href="#addproduct-img-collapse" class="text-body collbodyd" data-bs-toggle="collapse" aria-haspopup="true" aria-expanded="false" aria-haspopup="true" aria-controls="addproduct-img-collapse">
+                                    <div class="p-4">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-shrink-0 me-3">
+                                                <div class="avatar">
+                                                    <div class="avatar-title rounded-circle bg-dark-subtle  text-dark">
+                                                        <h5 class="text-dark font-size-17 mb-0">02</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="font-size-16 mb-1">Island Image</h5>
+                                                <p class="text-muted text-truncate mb-0">Fill all information below</p>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <i class="mdi mdi-chevron-up accor-down-icon font-size-24"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <div id="addproduct-img-collapse" class="collapse" data-bs-parent="#addproduct-accordion">
+                                    <div class="p-4 border-top">
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage1" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <br>
+                                                    <label class="form-label" for="isd_image1">Image 1</label>
+                                                    <input id="isd_image1" name="isd_image1" type="file" accept="image/*" class="form-control" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage2" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <label class="form-label" for="isd_image2">Image 2</label>
+                                                    <input id="isd_image2" name="isd_image2" type="file" accept="image/*" class="form-control" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage3" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <label class="form-label" for="isd_image3">Image 3</label>
+                                                    <input id="isd_image3" name="isd_image3" type="file" accept="image/*" class="form-control" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage4" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <label class="form-label" for="isd_image4">Image 4</label>
+                                                    <input id="isd_image4" name="isd_image4" type="file" accept="image/*" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage5" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <label class="form-label" for="isd_image5">Image 5</label>
+                                                    <input id="isd_image5" name="isd_image5" type="file" accept="image/*" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="mb-3">
+                                                    <center>
+                                                        <img class="rounded me-2" src="" id="previewImage6" data-holder-rendered="true" style="height: 100px; width:100px;">
+                                                    </center>
+                                                    <label class="form-label" for="isd_image6">Image 6</label>
+                                                    <input id="isd_image6" name="isd_image6" type="file" accept="image/*" class="form-control">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <a href="#addproduct-metadata-collapse" class="text-body collbodyd" data-bs-toggle="collapse" aria-haspopup="true" aria-expanded="false" aria-haspopup="true" aria-controls="addproduct-metadata-collapse">
+                                    <div class="p-4">
+
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-shrink-0 me-3">
+                                                <div class="avatar">
+                                                    <div class="avatar-title rounded-circle bg-dark-subtle  text-dark">
+                                                        <h5 class="text-dark font-size-17 mb-0">03</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="font-size-16 mb-1">Meta Data</h5>
+                                                <p class="text-muted text-truncate mb-0">Fill all information below</p>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <i class="mdi mdi-chevron-up accor-down-icon font-size-24"></i>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </a>
+
+                                <div id="addproduct-metadata-collapse" class="collapse" data-bs-parent="#addproduct-accordion">
+                                    <div class="p-4 border-top">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="isd_keyword">Keywords</label>
+                                            <input id="isd_keyword" name="isd_keyword" placeholder="Enter Keyword" type="text" class="form-control">
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_slug_en">Slug (en)</label>
+                                                    <input id="isd_slug_en" name="isd_slug_en" placeholder="Enter Island Slug" type="text" class="form-control" required>
+                                                    </input>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_slug_idn">Slug (idn)</label>
+                                                    <input id="isd_slug_idn" name="isd_slug_idn" placeholder="Enter Island Slug" type="text" class="form-control" required>
+                                                    </input>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_description_en">Description (en)</label>
+                                                    <textarea class="form-control" id="isd_description_en" name="isd_description_en" placeholder="Enter Description" rows="4"></textarea>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="isd_description_idn">Description (idn)</label>
+                                                    <textarea class="form-control" id="isd_description_idn" name="isd_description_idn" placeholder="Enter Description" rows="4"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Button --}}
+                <div class="row mb-4">
+                    <div class="col text-end">
+                        <button onclick="history.back()" class="btn btn-outline-dark"><i class="bx bx-x me-1"></i> Cancel</button>
+                        <button type="submit" class="btn btn-dark"><i class=" bx bx-file me-1"></i> Save</button>
+                    </div> <!-- end col -->
+                </div> <!-- end row-->
+            </form>
+            <!-- end row -->
+
+        </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+
+    @include('admin.components.footer')
+</div>
+@endsection
---
+++ resources/views/schedules/trip/index.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.admin_master') 
+@section('admin')
+
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap align-items-center mb-2">
+                                <h5 class="card-title">Trip Table</h5>
+                                <div class="ms-auto">
+                                    <div class="btn-toolbar float-end" role="toolbar">
+                                        <a href="{{route('trip.add')}}" class="btn btn-dark w-100" id="btn-new-event"><i class="mdi mdi-plus"></i> Create New Trip</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-striped table-centered align-middle table-nowrap mb-0 table-check">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Name</th>
+                                            <th>Status</th>
+                                            <th>Route</th>
+                                            <th>Fast Boat</th>
+                                            <th>Schedule</th>
+                                            <th>Departured Port</th>
+                                            <th>Arrival Port</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+    @include('admin.components.footer')
+</div>
+@endsection
