746c992002d7835825c70801b18a97404df756e9
ajil23 <azrilpraya@gmail.com>
2024-07-26 16:48:19 +0800

    folder view for trip & schedule

--- app/Http/Controllers/Backend/SchedulesScheduleController.php
+++ app/Http/Controllers/Backend/SchedulesScheduleController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\SchedulesSchedule;
 use Illuminate\Http\Request;
 
 class SchedulesScheduleController extends Controller
 {
-    //
+    public function index(){
+        $scheduleData = SchedulesSchedule::all();
+        return view('schedules.schedule.index', compact('scheduleData'));
+    }
 }
---
+++ resources/views/schedules/schedule/index.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.admin_master') 
+@section('admin')
+
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap align-items-center mb-2">
+                                <h5 class="card-title">Company Table</h5>
+                                <div class="ms-auto">
+                                    <div class="btn-toolbar float-end" role="toolbar">
+                                        <a href="{{route('schedule.add')}}" class="btn btn-dark w-100" id="btn-new-event"><i class="mdi mdi-plus"></i> Create New Company</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-striped table-centered align-middle table-nowrap mb-0 table-check">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Company</th>
+                                            <th>Name</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($scheduleData as $item)
+                                        <tr>
+                                            <td>{{$loop->iteration}}</td>
+                                            <td class="fw-semibold">{{$item->sch_company}}</td>
+                                            <td>{{$item->sch_name}}</td>
+                                            <td>
+                                                <div class="dropstart">
+                                                    <a class="text-muted dropdown-toggle font-size-18" role="button" data-bs-toggle="dropdown" aria-haspopup="true">
+                                                        <i class="mdi mdi-dots-horizontal"></i>
+                                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-end">
+                                                        <a class="dropdown-item" href="#">Edit</a>
+                                                        <a class="dropdown-item" data-confirm-delete="true" href="#" >Delete</a>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                    {{-- {{$scheduleData->links('pagination::bootstrap-5')}} --}}
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+    @include('admin.components.footer')
+</div>
+@endsection
