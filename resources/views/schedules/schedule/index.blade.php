4426736978d2438ff5033ae1ca8acd445d3e4072
Praya <109968393+ajil23@users.noreply.github.com>
2024-07-26 17:17:38 +0800

    rename schedule add button

--- resources/views/schedules/schedule/index.blade.php
+++ resources/views/schedules/schedule/index.blade.php
@@ -12,7 +12,7 @@
                                 <h5 class="card-title">Schedule Table</h5>
                                 <div class="ms-auto">
                                     <div class="btn-toolbar float-end" role="toolbar">
-                                        <a href="{{route('schedule.add')}}" class="btn btn-dark w-100" id="btn-new-event"><i class="mdi mdi-plus"></i> Create New Company</a>
+                                        <a href="{{route('schedule.add')}}" class="btn btn-dark w-100" id="btn-new-event"><i class="mdi mdi-plus"></i> Create New Schedule</a>
                                     </div>
                                 </div>
                             </div>
