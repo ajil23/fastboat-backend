e7576ef8d2611561167280d5519a353dd0dc348e
ajil23 <azrilpraya@gmail.com>
2024-07-31 10:18:21 +0800

    shuttle model

---
+++ app/Models/ScheduleShuttleArea.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScheduleShuttleArea extends Model
+{
+    use HasFactory;
+    protected $table = "schedulesshuttlearea";
+    protected $primaryKey = 'sa_id';
+    protected $fillable = ['sa_id', 'sa_island', 'sa_name', 'updated_by'];
+
+    public function island(){
+        return $this->belongsTo(MasterIsland::class,'sa_island','isd_id');
+    }
+}
