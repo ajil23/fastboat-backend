38bd2f9c8426480d22512990e8a07745ca45f31d
ernakha <ernakhasanah78@gmail.com>
2024-07-31 09:06:07 +0800

    create route, controller, database for Shuttle Area and Shuttle

---
+++ database/migrations/2024_07_31_080358_create_schedulesshuttlearea_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedulesshuttlearea', function (Blueprint $table) {
+            $table->id('sa_id');
+            $table->unsignedBigInteger('sa_island');
+            $table->foreign('sa_island')->references('isd_id')->on('masterisland')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('sa_name', 100);
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedulesshuttlearea');
+    }
+};
---
+++ database/migrations/2024_07_31_080406_create_schedulesshuttle_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedulesshuttle', function (Blueprint $table) {
+            $table->id('s_id');
+            $table->unsignedBigInteger('s_trip');
+            $table->foreign('s_trip')->references('fbt_id')->on('schedulestrip')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('s_area');
+            $table->foreign('s_area')->references('sa_id')->on('schedulesshuttlearea')->onDelete('cascade')->onUpdate('cascade');
+            $table->time('s_start');
+            $table->time('s_end');
+            $table->string('s_meeting_point', 100);
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedulesshuttle');
+    }
+};
