1ced25834196e668fa2d17a609e8f34330919e05
ajil23 <azrilpraya@gmail.com>
2024-08-05 11:02:52 +0800

    revision layout #4 for add data shuttel

--- resources/views/schedules/shuttle/add.blade.php
+++ resources/views/schedules/shuttle/add.blade.php
@@ -8,14 +8,14 @@
                 @csrf
                 <div class="row">
                     <div class="col-lg-12">
-                        <div id="addproduct-accordion" class="custom-accordion">
+                        <div id="addproduct-accordion">
                             <div class="card">
                                 <a class="text-body" data-bs-toggle="collapse" aria-expanded="true" aria-controls="addproduct-productinfo-collapse">
                                     <div class="p-4">
                                         <div class="d-flex align-items-center">
                                             <div class="flex-grow-1 overflow-hidden">
-                                                <h5 class="font-size-16 mb-1"> Shuttle Info</h5>
-                                                <p class="text-muted text-truncate mb-0">Fill all information below</p>
+                                                <h5 class="font-size-16 mb-1">Shuttle Search</h5>
+                                                <p class="text-muted text-truncate mb-0">Search the shuttle</p>
                                             </div>
                                         </div>
                                     </div>
@@ -93,6 +93,86 @@
                             <!-- end card -->
                         </div>
                         <!-- end col -->
+                    </div>
+                    <div class="col-lg-12">
+                        <div id="addproduct-accordion">
+                            <div class="card">
+                                <a class="text-body" data-bs-toggle="collapse" aria-expanded="true" aria-controls="addproduct-productinfo-collapse">
+                                    {{-- add function to select all checkbox --}}
+                                    <div class="p-4">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <h5 class="font-size-16 mb-1"> Shuttle Info</h5>
+                                                <p class="text-muted text-truncate mb-0">Fill all information below</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <div id="addproduct-productinfo-collapse" class="collapse show">
+                                    <table class="table mb-0">
+                                        <thead class="table-light">
+                                            <tr>
+                                                <th scope="col" class="ps-4" style="width: 50px;">
+                                                    <div class="form-check font-size-16">
+                                                        <input type="checkbox" class="checkedbox" id="sa_id" onclick="toggleSelectAll(this)">
+                                                    </div>
+                                                </th>
+                                                <th><center>No</center></th>
+                                                <th><center>Area</center></th>
+                                                <th><center>Start</center></th>
+                                                <th><center>End</center></th>
+                                                <th><center>Meeting Point</center></th>
+                                                <th><center>Note</center></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row" class="ps-4">
+                                                    <div class="form-check font-size-16">
+                                                        <input type="checkbox" class="checkedbox" name="#">
+                                                    </div>
+                                                </th>
+                                                <td><center>1</center></td>
+                                                <td><center>Canggu</center></td>
+                                                <td>
+                                                    <center>
+                                                        <select class="form-control" data-trigger name="cpn_type" id="cpn_type" required>
+                                                            <option value="">Select</option>
+                                                            <option value="fast_boat">Fast Boat</option>
+                                                            <option value="car_transfer">Car Transfer</option>
+                                                            <option value="yacht">Yacht</option>
+                                                            <option value="tour">Tour</option>
+                                                        </select>
+                                                    </center>
+                                                </td>
+                                                <td>
+                                                    <center>
+                                                        <select class="form-control" data-trigger name="cpn_type" id="cpn_type" required>
+                                                            <option value="">Select</option>
+                                                            <option value="fast_boat">Fast Boat</option>
+                                                            <option value="car_transfer">Car Transfer</option>
+                                                            <option value="yacht">Yacht</option>
+                                                            <option value="tour">Tour</option>
+                                                        </select>
+                                                    </center>
+                                                </td>
+                                                <td>
+                                                   <center>
+                                                    <input type="checkbox" id="switch9" switch="dark" checked />
+                                                    <label for="switch9" data-on-label="Yes" data-off-label="No"></label>
+                                                   </center>
+                                                </td>
+                                                <td>
+                                                    <center>
+                                                        <input id="s_meeting_point" name="s_meeting_point" placeholder="Note/Meeting Point Location" type="text" class="form-control" ></input>
+                                                    </center>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
