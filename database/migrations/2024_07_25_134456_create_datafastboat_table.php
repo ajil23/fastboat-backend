06a33eacecc09ad872e0e6e04951342108a2f4a3
ernakha <ernakhasanah78@gmail.com>
2024-08-12 16:25:01 +0700

    fixing database

--- database/migrations/2024_07_25_134456_create_datafastboat_table.php
+++ database/migrations/2024_07_25_134456_create_datafastboat_table.php
@@ -29,6 +29,8 @@ return new class extends Migration
             $table->text('fb_description_idn');
             $table->longText('fb_content_en');
             $table->longText('fb_content_idn');
+            $table->longText('prt_term_condition_en')->nullable();
+            $table->longText('prt_term_condition_idn')->nullable();
             $table->integer('fb_status')->default(1);
             $table->string('fb_updated_by');
             $table->timestamps();
