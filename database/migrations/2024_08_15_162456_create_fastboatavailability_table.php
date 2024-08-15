067b756067a3a75c92de201cea76023256df553b
ernakha <ernakhasanah78@gmail.com>
2024-08-15 16:54:21 +0700

    make database, model, controller, and view for availability

---
+++ database/migrations/2024_08_15_162456_create_fastboatavailability_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fastboatavailability', function (Blueprint $table) {
+            $table->id('fba_id');
+            $table->unsignedBigInteger('fba_trip_id');
+            $table->foreign('fba_trip_id')->references('fbt_id')->on('schedulestrip')->onDelete('cascade')->onUpdate('cascade');
+            $table->date('fba_date');
+            $table->time('fba_dept_time')->nullable();
+            $table->time('fba_arriv_time')->nullable();
+            $table->string('fba_adult_nett', 11)->nullable();
+            $table->string('fba_child_nett', 11)->nullable();
+            $table->string('fba_adult_publish', 11)->nullable();
+            $table->string('fba_child_publish', 11)->nullable();
+            $table->string('fba_discount', 11)->nullable();
+            $table->string('fba_stock', 11)->nullable();
+            $table->string('status');
+            $table->string('fba_shuttle_status');
+            $table->text('info');
+            $table->string('fba_created_by');
+            $table->string('fba_updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fastboatavailability');
+    }
+};
---
+++ resources/views/fast-boat/availability/index.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap align-items-center mb-2">
+                                <h5 class="card-title">Fast Boat Table</h5>
+                                <div class="ms-auto">
+                                    <div class="btn-toolbar float-end" role="toolbar">
+                                        <a href="{{route('availability.add')}}" class="btn btn-dark w-100" id="btn-new-event"><i class="mdi mdi-plus"></i>Fast Boat</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table table-striped table-centered align-middle table-nowrap mb-0 table-check">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th style="width: 90px;">
+                                                Picture
+                                            </th>
+                                            <th style="width: 210px;">Fast Boat Name</th>
+                                            <th>Company Name</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+
+
+    @include('admin.components.footer')
+</div>
+@endsection
+
