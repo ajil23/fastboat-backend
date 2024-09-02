a74edcce9a6f9c036dbd26ad27bdc5e0dfe32379
ajil23 <azrilpraya@gmail.com>
2024-09-02 17:34:14 +0800

    fixing fast boat api

--- app/Http/Controllers/Api/FastboatApiController.php
+++ app/Http/Controllers/Api/FastboatApiController.php
@@ -21,15 +21,6 @@ class FastboatApiController extends Controller
         ], 200);
     }
 
-    public function paginate()
-    {
-        $data = DataFastboat::paginate(4);
-
-        return response()->json([
-            'data' => $data,
-        ]);
-    }
-
     public function show_en($fb_slug_en)
     {
         // Mencari post berdasarkan slug dan memuat data terkait dari tabel ScheduleTrip
@@ -76,15 +67,13 @@ class FastboatApiController extends Controller
 
     public function show_idn($fb_slug_idn)
     {
-        // Mencari post berdasarkan slug
-        $data = DataFastboat::where('fb_slug_idn', $fb_slug_idn)->first();
+        // Mencari post berdasarkan slug dan memuat data terkait dari tabel ScheduleTrip
+        $data = DataFastboat::with('scheduleTrips.deptPort', 'scheduleTrips.arrivalPort')
+            ->where('fb_slug_en', $fb_slug_idn)
+            ->first();
 
         if (!$data) {
-            return response()->json(['message' => 'Data tidak ditemukan'], 404);
-        }
-
-        if ($data->scheduleTrips->isEmpty()) {
-            return response()->json(['message' => 'Jadwal tidak ditemukan'], 404);
+            return response()->json(['message' => 'Data not found'], 404);
         }
 
         $result = [
@@ -98,7 +87,11 @@ class FastboatApiController extends Controller
             'fb_image4' => $data->fb_image4,
             'fb_image5' => $data->fb_image5,
             'fb_image6' => $data->fb_image6,
-            'schedule_trips' => $data->scheduleTrips->map(function ($scheduleTrip) {
+        ];
+
+        // Menambahkan schedule_trips jika ada
+        if ($data->scheduleTrips->isNotEmpty()) {
+            $result['schedule_trips'] = $data->scheduleTrips->map(function ($scheduleTrip) {
                 return [
                     'fbt_id' => $scheduleTrip->fbt_id,
                     'fbt_name' => $scheduleTrip->fbt_name,
@@ -106,9 +99,11 @@ class FastboatApiController extends Controller
                     'departure_time' => $scheduleTrip->fbt_dept_time,
                     'arrival_port' => $scheduleTrip->arrivalPort ? $scheduleTrip->arrivalPort->prt_name_idn : null,
                     'arrival_time' => $scheduleTrip->fbt_arrival_time,
+                    'departure_port_slug' => $scheduleTrip->deptPort ? $scheduleTrip->deptPort->prt_slug_idn : null,
+                    'arrival_port_slug' => $scheduleTrip->arrivalPort ? $scheduleTrip->arrivalPort->prt_slug_idn : null,
                 ];
-            })
-        ];
+            });
+        }
 
         return response()->json($result, 200);
     }
