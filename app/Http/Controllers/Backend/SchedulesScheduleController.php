58f87e8b57aa1dd298a3fc1c657d3cabdaab633c
ajil23 <azrilpraya@gmail.com>
2024-09-04 09:49:27 +0800

    schedule update with modal

--- app/Http/Controllers/Backend/SchedulesScheduleController.php
+++ app/Http/Controllers/Backend/SchedulesScheduleController.php
@@ -10,7 +10,8 @@ use Illuminate\Http\Request;
 class SchedulesScheduleController extends Controller
 {
     // this function is for view all data from schedule table
-    public function index(){
+    public function index()
+    {
         $scheduleData = SchedulesSchedule::all();
         $company = DataCompany::all();
         $title = 'Delete Schedule Data!';
@@ -20,7 +21,8 @@ class SchedulesScheduleController extends Controller
     }
 
     // this function will request data from input in schedule add form
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // Handle the request data validation
         $request->validate([
             'sch_name' => 'required',
@@ -29,33 +31,38 @@ class SchedulesScheduleController extends Controller
 
         // Handle insert data to database
         $scheduleData = new SchedulesSchedule();
-        $scheduleData -> sch_company = $request->sch_company;
-        $scheduleData -> sch_name = $request->sch_name;
-        $scheduleData -> save();
+        $scheduleData->sch_company = $request->sch_company;
+        $scheduleData->sch_name = $request->sch_name;
+        $scheduleData->sch_updated_by = Auth()->id();
+        $scheduleData->save();
         toast('Your data as been submited!', 'success');
         return redirect()->route('schedule.view');
     }
-    
+
     // this function will get the $id of the selected data and then view the schedule edit form
-    public function edit($id){
+    public function edit($id)
+    {
         $scheduleData = SchedulesSchedule::find($id);
         $company = DataCompany::all();
-        return view('schedules.schedule.edit', compact('scheduleData', 'company'));
+        return response()->json($scheduleData, $company);
     }
 
     //this function will get the $id of the selected data and request data from input in schedule edit from 
-    public function update(Request $request, $id){
-        // Handle insert data to database
-        $scheduleData = SchedulesSchedule::find($id);
-        $scheduleData -> sch_company = $request->sch_company;
-        $scheduleData -> sch_name = $request->sch_name;
-        $scheduleData -> update();
+    public function update(Request $request, $id)
+    {
+        $schedule = SchedulesSchedule::findOrFail($id);
+        $schedule->sch_company = $request->sch_company;
+        $schedule->sch_name = $request->sch_name;
+        $schedule->sch_updated_by = Auth()->id();
+        $schedule->update();
         toast('Your data as been edited!', 'success');
         return redirect()->route('schedule.view');
     }
 
+
     // this function will get the $id of selected data and do delete operation
-    public function delete($id){
+    public function delete($id)
+    {
         $scheduleData = SchedulesSchedule::find($id);
         $scheduleData->delete();
         toast('Your data as been deleted!', 'success');
--- resources/views/schedules/schedule/index.blade.php
+++ resources/views/schedules/schedule/index.blade.php
@@ -38,22 +38,24 @@
                                         <tr id="baris-{{$item->sch_id}}" class="search">
                                             <td>{{$loop->iteration}}</td>
                                             <td>{{$item->company->cpn_name}}</td>
-                                            <td class="search-item"  data-id="{{$item->sch_id}}">{{$item->sch_name}}</td>
+                                            <td class="search-item" data-id="{{$item->sch_id}}">{{$item->sch_name}}</td>
                                             <td>
                                                 <div class="dropstart">
                                                     <a class="text-muted dropdown-toggle font-size-18" role="button" data-bs-toggle="dropdown" aria-haspopup="true">
                                                         <i class="mdi mdi-dots-horizontal"></i>
                                                     </a>
                                                     <div class="dropdown-menu dropdown-menu-end">
-                                                        <a class="dropdown-item" href="{{route('schedule.edit', $item->sch_id)}}">Edit</a>
+                                                        <a class="dropdown-item" href="#" data-bs-toggle="modal"
+                                                        data-bs-target="#editDataModal" data-url="{{route('schedule.edit', $item->sch_id)}}">Edit</a>                                                        
                                                         <a class="dropdown-item" data-confirm-delete="true" href="{{route('schedule.delete', $item->sch_id)}}" >Delete</a>
                                                     </div>
                                                 </div>
                                             </td>
+                                            <input type="hidden" class="schedule-id" value="{{$item->sch_id}}">
+                                            <input type="hidden" class="company-id" value="{{$item->company->cpn_id}}">
                                         </tr>
                                         @endforeach
                                     </tbody>
-                                    {{-- {{$scheduleData->links('pagination::bootstrap-5')}} --}}
                                 </table>
                             </div>
                         </div>
@@ -65,7 +67,7 @@
     </div>
     <!-- End Page-content -->
 
-     <!-- Adding data modal -->
+    <!-- Adding data modal -->
     <div class="modal fade" id="addDataModal" tabindex="-1" role="dialog"
         aria-labelledby="addDataModalTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable">
@@ -99,38 +101,97 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
+    <!-- Edit data modal -->
+    <div class="modal fade" id="editDataModal" tabindex="-1" role="dialog"
+        aria-labelledby="editDataModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editDataModalTitle">Edit Schedule</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="editScheduleForm" action="" method="post">
+                        @csrf
+                        @method('POST')
+                        <div class="mb-3">
+                            <label for="sch_company" class="form-label">Company</label>
+                            <select data-trigger name="sch_company" class="form-control" id="sch_company_edit" required>
+                                @foreach ($company as $item)
+                                    <option value="{{$item->cpn_id}}">{{$item->cpn_name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="sch_name" class="form-label">Schedule Name</label>
+                            <input type="text" class="form-control" name="sch_name" id="sch_name_edit" placeholder="Type the schedule name" required>
+                        </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-dark">Save</button>
+                        </div>
+                    </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
     @include('admin.components.footer')
 </div>
 @endsection
 
 @section('script')
-<!-- search box -->
 <script>
+    // search box
     $(document).ready(function() {
         $('#search-input').on('input', function() {
             var search= $(this).val();
             var lowerCaseText = search.toLowerCase();
             var list = $('.search-item');
-            // console.log(list);
-            $('.search').show()
+            $('.search').show();
             list.each(function(){
                 var item = $(this).text();
                 var id = $(this).data('id');
                 if(item.toLowerCase().includes(lowerCaseText)===false){
-                    $('#baris-'+id).hide()
+                    $('#baris-'+id).hide();
                 }
-            })
-        })
-    })
-</script>
+            });
+        });
+    });
 
-{{-- tom select --}}
-<script>
-    new TomSelect("#sch_company",{
-	    sortField: {
-		field: "text",
-		direction: "asc"
-	    }
+        // tom select
+        new TomSelect("#sch_company",{
+        sortField: {
+            field: "text",
+            direction: "asc"
+        }
+    });
+
+    // edit modal
+    $(document).ready(function() {
+        // Event handler untuk tombol edit
+        $('body').on('click', '.dropdown-item[data-bs-target="#editDataModal"]', function() {
+            // Ambil ID dan data lain dari baris yang dipilih
+            var tr = $(this).closest('tr');
+            var id = tr.attr('id').replace('baris-', '');
+            var scheduleName = tr.find('.search-item').text();
+            var companyId = tr.find('.company-id').val();
+            var scheduleId = tr.find('.schedule-id').val();
+            
+            // Isi modal dengan data yang sesuai
+            $('#editDataModal #sch_name_edit').val(scheduleName);
+            $('#editDataModal #sch_company_edit').val(companyId);
+    
+            // Ubah action form untuk update sesuai dengan id
+            var formAction = '{{ route("schedule.update", ":id") }}';
+            formAction = formAction.replace(':id', scheduleId);
+
+            $('#editScheduleForm').attr('action', formAction);
+    
+            // Set value dropdown secara manual dan trigger change
+            $('#sch_company_edit').val(companyId).trigger('change');
+        });
     });
 </script>
+
 @endsection
